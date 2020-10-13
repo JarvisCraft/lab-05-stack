e76d73da3d24db156366cdc1196b454d69ed4c58
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-14 01:18:27 +0300

    feat: fully implement Stack

--- include/simple_stack.hpp
+++ include/simple_stack.hpp
@@ -4,6 +4,7 @@
 #define INCLUDE_SIMPLE_STACK_HPP_
 
 #include <cstddef>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 
@@ -31,23 +32,25 @@ namespace simple_stack {
 
         Stack& operator=(Stack&& original) noexcept;
 
+        [[nodiscard]] bool empty() const;
+
         [[nodiscard]] ::std::size_t size() const;
 
         [[nodiscard]] const_reference_t head() const;
 
-        void push(rvalue_reference_t value);
-
         void push(const_reference_t value);
 
-        void pop();
+        void push(rvalue_reference_t value);
 
         template <typename... Args_T>
         void push_emplace(Args_T&&... args);
 
+        void pop();
+
     private:
         struct Node {
             value_t value;
-            pointer_t previous, next;
+            Node *previous, *next;
         };
 
         ::std::size_t size_;
@@ -82,6 +85,95 @@ namespace simple_stack {
         }
 
         return *this;
+    }
+
+    template <typename Value_T>
+    bool Stack<Value_T>::empty() const {
+        return size_ == 0;
+    }
+
+    template <typename Value_T>
+    ::std::size_t Stack<Value_T>::size() const {
+        return size_;
+    }
+
+    template <typename Value_T>
+    typename Stack<Value_T>::const_reference_t Stack<Value_T>::head() const {
+        auto const head = head_;
+        if (head == nullptr) throw ::std::out_of_range("attempt to call `head() const` on empty stack");
+
+        return head_->value;
+    }
+
+    template <typename Value_T>
+    void Stack<Value_T>::push(const_reference_t value) {
+        static_assert(::std::is_copy_constructible<value_t>::value);
+
+        if (size_ == 0) {
+            tail_ = head_ = new Node{::std::move(value), nullptr, nullptr};
+            size_ = 1;
+        } else {
+            head_ = new Node{::std::move(value), head_, nullptr};
+            ++size_;
+        }
+    }
+
+    template <typename Value_T>
+    void Stack<Value_T>::push(rvalue_reference_t value) {
+        static_assert(::std::is_move_constructible<value_t>::value);
+
+        if (size_ == 0) {
+            tail_ = head_ = new Node{value, nullptr, nullptr};
+            size_ = 1;
+        } else {
+            auto const old_head = head_;
+            old_head->next = head_ = new Node{value, head_, nullptr};
+            ++size_;
+        }
+    }
+
+    template <typename Value_T>
+    template <typename... Args_T>
+    void Stack<Value_T>::push_emplace(Args_T&&... args) {
+        if (size_ == 0) {
+            tail_ = head_ = new Node{Value_T{::std::forward<Args_T>(args)...}, nullptr, nullptr};
+            size_ = 1;
+        } else {
+            auto const old_head = head_;
+            old_head->next = head_ = new Node{Value_T{::std::forward<Args_T>(args)...}, head_, nullptr};
+            ++size_;
+        }
+    }
+
+    template <typename Value_T>
+    void Stack<Value_T>::pop() {
+        auto const size = size_;
+        switch (size) {
+            case 0: return; // no-op
+            case 1: {
+                delete head_;
+
+                tail_ = head_ = nullptr;
+                size_ = 0;
+
+                return;
+            }
+            case 2: {
+                delete head_;
+
+                (head_ = tail_)->next = nullptr;
+                size_ = 1;
+
+                return;
+            }
+            default: {
+                auto const old_head = head_;
+                head_ = old_head->previous;
+                delete old_head;
+
+                size_ = size - 1;
+            }
+        }
     }
 } // namespace simple_stack
 
--- sources/simple_stack.cpp
+++ sources/simple_stack.cpp
@@ -1,3 +1,1 @@
 // Copyright 2020 Petr Portnov <gh@progrm-jarvis.ru>
-
-#include <simple_stack.hpp>
